86c2434a8dd3978be2b0a14ab70a35b206302dc3
Durgesh Wankhede <durgeshfore@gmail.com>
2024-07-24 15:52:57 +0530

    Day 1 - Python Bootcamp

---
+++ Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to The Band Name Generator.")
+City_name = input("Which City did you grow up in ?\n")
+Pet_name = input("What is the name of your pet ?\n")
+
+print("Your band name could be " + City_name + " " + Pet_name)
